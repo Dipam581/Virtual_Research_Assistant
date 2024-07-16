8cba494e87a5092af23d6318c0b6540d69277a27
Dipam Ghosh <107043079+Dipam581@users.noreply.github.com>
2024-07-16 13:10:20 +0530

    summary

--- frontend.py
+++ frontend.py
@@ -1,23 +1,20 @@
 import streamlit as st
 import time
-from pdfDataExtracter import read_pdf
+from pdfDataExtracter import read_pdf, explain_query
 
 st.set_page_config(
     page_title="VRA",
     page_icon="📖",
 )
 st.header("Welcome to Virtual Research Assistant! 👋")
-st.sidebar.success("Select a service.")
+st.sidebar.success("Select Your Assistant.")
 
-
+pdf_response = ""
 def upload_PDF():
     pdf_file = st.file_uploader("Choose a PDF file", accept_multiple_files=False)
-    st.spinner('Data is processing...')
-    response = read_pdf(pdf_file)
-    st.write(response)
-
-    if response:
-        st.text_input("Type any queries regarding this content")
+    with st.spinner('Data is processing...'):
+        pdf_response = read_pdf(pdf_file)
+        st.write(pdf_response)
 
 
 def upload_DOC():
@@ -34,3 +31,9 @@ page_names_to_funcs = {
 
 demo_name = st.sidebar.selectbox("Choose a demo", page_names_to_funcs.keys())
 page_names_to_funcs[demo_name]()
+
+if pdf_response:
+    query = st.text_input("Type any queries regarding this content")
+    if st.button("Explain"):
+        if query:
+            explain_query(query)
--- pdfDataExtracter.py
+++ pdfDataExtracter.py
@@ -1,72 +1,85 @@
 from PyPDF2 import PdfReader
 import re, os
-from langchain.vectorstores import Chroma
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import Pinecone
-import chromadb.utils.embedding_functions as embedding_functions
+
+from langchain.chains.question_answering import load_qa_chain
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Pinecone
+from langchain_huggingface import HuggingFaceEndpoint
+from langchain_text_splitters import RecursiveCharacterTextSplitter, RecursiveJsonSplitter
 from pinecone import Pinecone, ServerlessSpec
 from langchain_pinecone import PineconeVectorStore
 from transformers import pipeline
 
 
-class Document:
-    def __init__(self, content, metadata=None):
-        self.page_content = content
-        self.metadata = metadata or {}
+class DocumentPage:
+    def __init__(self, page_content):
+        self.page_content = page_content
 
+def chunkData(docs,chunk_size=200,chunk_overlap=30):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size,chunk_overlap=chunk_overlap,length_function=len,is_separator_regex=True)
+    doc = text_splitter.create_documents([docs])
+    return doc
 
 def embed_document(document):
     os.environ['PINECONE_API_KEY'] = "356688b7-fc9b-49ba-9c5f-7162954577cd"
-    pc = Pinecone(api_key="356688b7-fc9b-49ba-9c5f-7162954577cd")
     summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-    # pc.create_index(
-    #     name="VRA",
-    #     dimension=2,  # Replace with your model dimensions
-    #     metric="cosine",  # Replace with your model metric
-    #     spec=ServerlessSpec(
-    #         cloud="aws",
-    #         region="us-east-1"
-    #     )
-    # )
-    # index = PineconeVectorStore.from_documents(document, index_name="VRA", embedding=huggingface_ef)
-    # huggingface_ef = embedding_functions.HuggingFaceEmbeddingFunction(
-    #     api_key="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",
-    #     model_name="facebook/bart-large-cnn"
-    # )
-    print("start")
+
     response = (summarizer(document, max_length=130, min_length=30, do_sample=False))
 
-    document = ""
-
+    mod_document = ""
     for idx, item in enumerate(response):
         summary_text = item.get('summary_text', '')
-        document += summary_text
+        mod_document += summary_text
         if idx < len(response) - 1:
-            document += "\n\n"
-    return document
+            mod_document += "\n\n"
 
+    documents = chunkData(docs=mod_document)
+    embedding = HuggingFaceEmbeddings()
+    pc = Pinecone(api_key=os.environ.get("PINECONE_API_KEY"))
 
+    index_name = "vradocs"
 
+    if index_name not in pc.list_indexes().names():
+        pc.create_index(
+            name=index_name,
+            dimension=768,
+            metric="cosine",
+            spec=ServerlessSpec(
+                cloud="aws",
+                region="us-east-1"
+            )
+        )
 
+    index = PineconeVectorStore.from_documents(documents, index_name=index_name, embedding=embedding)
+
+    return mod_document
+
+corpus = []
 def read_pdf(data):
-    # try:
-    #     if data:
-    #         corpus = []
-    #         reader = PdfReader(data)
-    #         for page in reader.pages:
-    #             py_data = re.sub('[^a-zA-Z0-9]', " ", page.extract_text())
-    #             embed_document(py_data)
-    #             corpus.append(py_data)
-    #
-    # except:
-    #     print("Error in reading documents")
-    # finally:
-    #     return
     if data:
-        corpus = []
+
         reader = PdfReader(data)
         for page_number, page in enumerate(reader.pages):
             py_data = re.sub('[^a-zA-Z0-9]', " ", page.extract_text())
             corpus.append(py_data)
 
         return embed_document(corpus)
+
+
+def explain_query(query):
+    print("query: {}".format(query))
+    print(corpus)
+    os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
+    repo_id = "mistralai/Mistral-7B-Instruct-v0.2"
+    llm_hug = HuggingFaceEndpoint(
+        repo_id=repo_id, max_length=128, temperature=0.9, token="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"
+    )
+    chain = load_qa_chain(llm_hug, chain_type="stuff")
+
+    def retrivequery(query, k=2):
+        matching = index.similarity_search(query=query, k=k)
+        return matching
+    def retriveAnswer(query):
+        doc_search = retrivequery(query)
+        response = chain.run(input_documents=doc_search, question=query)
+        return response
